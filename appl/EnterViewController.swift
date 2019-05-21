ad61f30953f77684bc3c8bb65bafa360290ee3b0
kirill404 <lamokk@mail.ru>
2019-05-21 12:28:13 +0300

    Сделал второе ДЗ.

---
+++ appl/EnterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EnterViewController.swift
+//  views
+//
+//  Created by Кирилл Пучков on 21/05/2019.
+//  Copyright © 2019 Кирилл Пучков. All rights reserved.
+//
+
+import UIKit
+
+class EnterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
